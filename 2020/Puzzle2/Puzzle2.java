5a7c2a444a9e72b89b157f615c7c64c2603fa7f5
AyameU <54376512+AyameU@users.noreply.github.com>
2020-12-02 08:29:52 -0600

    Solution for Puzzle 2 part 1, lots of String methods

--- 2020/Puzzle2/Puzzle2.java
+++ 2020/Puzzle2/Puzzle2.java
@@ -6,36 +6,54 @@ public class Puzzle2 {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        File file = new File("inputtest.txt");
+        File file = new File("input.txt");
         Scanner reader = new Scanner(file);
 
         // Used to store valid passwords
         ArrayList<String> valid = new ArrayList<String>();
 
-        // Loop through each line in the file
-        // Break into substrings
+        // Loop through each line in the file. Break into substrings
         while(reader.hasNextLine()) {
             // Get whole line, store as a String.
             String line = reader.nextLine();
-            System.out.println(line);
+            //System.out.println(line);
 
-            // Get the password as a substring.
+            // Get the password as a substring. Turn password into char array
             String password = line.substring(line.indexOf(":") + 2);
-            System.out.println(password);
+            char[] passwordChars = password.toCharArray();
+            //System.out.println(password);
 
-            // Get the character we will be comparing against.
+            // Get the character we will be counting.
             char character = line.charAt(line.indexOf(":") - 1);
-            System.out.println(character);
+            //System.out.println(character);
 
             // Get the min digits
             int min = Integer.parseInt(line.substring(0, line.indexOf("-")));
-            System.out.println(min);
+            //System.out.println(min);
 
             // Get the max digits
             int max = Integer.parseInt(line.substring(line.indexOf("-") + 1, line.indexOf(" ")));
-            System.out.println(max);
+            //System.out.println(max);
 
             // Using these pieces figure out solution.
+            // Count instances of the key character.
+            int numOfLetter = 0;
+
+            // Loop through each character in the password.
+            for(int i = 0; i < passwordChars.length; i++) {
+                if(passwordChars[i] == character) {
+                    numOfLetter++;
+                }
+            }
+
+            // Check numOfLetters against min/max values
+            // If between or equal to those values, add to array
+            if(numOfLetter >= min && numOfLetter <= max) {
+                valid.add(password);
+            }
         }
+
+        // Print number of valid passwords
+        System.out.println("Valid passwords: " + valid.size());
     }
 }
